60d2e47efb76844fcfcb209b7ef2f05a2e928ce4
muzakparov <muzakparov@gmail.com>
2020-01-19 10:16:02 +0300

    Dad Jokes Generator

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-const progressDone = document.querySelector(".progress-done");
+const jokeEl = document.getElementById("joke");
+const get_joke = document.getElementById("get_joke");
 
-setTimeout(() => {
-  progressDone.style.width = progressDone.getAttribute("data-done") + "%";
-  progressDone.style.opacity = 1;
-}, 500);
+get_joke.addEventListener("click", generateJoke);
+
+generateJoke();
+
+async function generateJoke() {
+  const jokeRes = await fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json"
+    }
+  });
+  const joke = await jokeRes.json();
+  jokeEl.innerHTML = joke.joke;
+}
