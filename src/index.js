327e14423853285d852f8a58b3c2ec6ff1a5b03e
muzakparov <muzakparov@gmail.com>
2020-01-20 07:50:59 +0300

    Hover Board

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
-const circles = document.querySelectorAll(".circle");
-let activeLight = 0;
+const container = document.getElementById("container");
+const colors = ["#e74c3c", "#8e44ad", "#3498db", "#e67e22", "#2ecc71"];
+const SQUARES_NR = 500;
 
-setInterval(() => {
-  changeLight();
-}, 1000);
+for (let i = 0; i < SQUARES_NR; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
 
-function changeLight() {
-  circles[activeLight].className = "circle";
-  activeLight++;
+  square.addEventListener("mouseover", () => {
+    setColorToEl(square);
+  });
 
-  if (activeLight > 2) {
-    activeLight = 0;
-  }
+  square.addEventListener("mouseout", () => {
+    removeColorFromEl(square);
+  });
 
-  const currentLight = circles[activeLight];
+  container.appendChild(square);
+}
 
-  currentLight.classList.add(currentLight.getAttribute("color"));
+function setColorToEl(element) {
+  const color = getRandomColor();
+  element.style.background = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColorFromEl(element) {
+  element.style.background = "#1d1d1d";
+  element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
 }
