42806c2eabb6527ab3dcd8118b3a4506c329c41f
muzakparov <muzakparov@gmail.com>
2020-01-20 08:40:31 +0300

    Auto Text Effec

--- src/index.js
+++ src/index.js
@@ -1,33 +1,15 @@
-const container = document.getElementById("container");
-const colors = ["#e74c3c", "#8e44ad", "#3498db", "#e67e22", "#2ecc71"];
-const SQUARES_NR = 500;
+const text = document.getElementById("text");
+const prog = "We Love Programming!";
+let idx = 1;
 
-for (let i = 0; i < SQUARES_NR; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
+setInterval(writeText, 100);
 
-  square.addEventListener("mouseover", () => {
-    setColorToEl(square);
-  });
+function writeText() {
+  text.innerText = prog.slice(0, idx);
 
-  square.addEventListener("mouseout", () => {
-    removeColorFromEl(square);
-  });
+  idx++;
 
-  container.appendChild(square);
-}
-
-function setColorToEl(element) {
-  const color = getRandomColor();
-  element.style.background = color;
-  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
-}
-
-function removeColorFromEl(element) {
-  element.style.background = "#1d1d1d";
-  element.style.boxShadow = `0 0 2px #000`;
-}
-
-function getRandomColor() {
-  return colors[Math.floor(Math.random() * colors.length)];
+  if (idx > prog.length) {
+    idx = 1;
+  }
 }
